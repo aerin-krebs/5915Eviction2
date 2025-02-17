33af225b4bba89b7a8644a5864173f0bf5f24df4
miacline <145244170+miacline@users.noreply.github.com>
2025-02-17 12:13:58 -0500

    Home styling updates

--- eviction2app/src/app/components/Header.js
+++ eviction2app/src/app/components/Header.js
@@ -1,6 +1,5 @@
 'use client';
 
-// import Link from 'next/link';
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 import { motion } from 'framer-motion';
@@ -12,7 +11,7 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>EvictionEducation</h1>
+      <h1 className={styles.title}>EvictionEducation.</h1>
       <div className={styles.menuButton}>
         <button onClick={() => setMenuOpen(!menuOpen)}>
           {menuOpen ? <X size={24} /> : <Menu size={24} />}
@@ -20,9 +19,10 @@ export default function Header() {
       </div>
       <div className={styles.navLinks}>
         <Link href="/" underline="hover" color=" #1d3747" className={styles.navLink}>Home</Link>
-        <Link href="/resource-finder" underline="hover" color=" #1d3747" className={styles.navLink}>Resources</Link>
         <Link href="/decision-finder" underline="hover" color=" #1d3747" className={styles.navLink}>Find A Solution</Link>
+        <Link href="/chat" underline="hover" color=" #1d3747" className={styles.navLink}>Chat</Link>
         <Link href="/faq" underline="hover" color=" #1d3747" className={styles.navLink}>FAQs</Link>
+        <Link href="/resource-finder" underline="hover" color=" #1d3747" className={styles.navLink}>Resources</Link>
         <Link href="/" underline="hover" color=" #1d3747" className={styles.navLink}>Translate</Link>
       </div>
       {menuOpen && (
@@ -31,9 +31,10 @@ export default function Header() {
           animate={{ opacity: 1, y: 0 }}
           className={styles.mobileMenu}>
           <Link href="/" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Home</Link>
-          <Link href="/resource-finder" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Resources</Link>
           <Link href="/decision-finder" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Find A Solution</Link>
+          <Link href="/chat" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Chat</Link>
           <Link href="/faq" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>FAQs</Link>
+          <Link href="/resource-finder" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Resources</Link>
           <Link href="/" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Translate</Link>
         </motion.div>
       )}
--- eviction2app/src/app/page.js
+++ eviction2app/src/app/page.js
@@ -10,27 +10,47 @@ export default function Home() {
     <div className={styles.container}>
       <Header />
       <div className={styles.contentBox}>
-        <h1 className={styles.title}>Facing Eviction?</h1>
+        <p className={styles.title}>Are You Facing Eviction?</p>
         <p className={styles.text}>No one should lose their home. Find the help you need today.</p>
-        
-        <p className={styles.text}>Need help finding a solution?</p>
-        <Button color="primary" component={Link} href="/decision-finder">
+        <Button
+          component={Link} 
+          href="/decision-finder"
+          variant="contained"
+          sx={{ backgroundColor: '#ffc844', color: '#1d3747', borderRadius: '50px', padding: '12px 24px', fontWeight: 'bold', margin: '1rem'}}
+        >
           Find A Solution
         </Button>
 
-        <p className={styles.text}>Have legal questions?</p>
+        <p className={styles.title}>Have legal questions?</p>
         <p className={styles.text}>Chat with Franklin County Self Help Center. Staff cannot offer legal advice but can assist in finding helpful resources and provide legal information.</p>
-        <Button color="primary" component={Link} href="/chat">
+        <Button 
+          component={Link} 
+          href="/chat"
+          variant="contained"
+          sx={{ backgroundColor: '#ffc844', color: '#1d3747', borderRadius: '50px', padding: '12px 24px', fontWeight: 'bold', margin: '1rem' }}
+        >
           Chat Now
         </Button>
 
-        <p className={styles.text}>View frequently asked questions</p>
-        <Button color="primary" component={Link} href="/faq">
+        <p className={styles.title}>View frequently asked questions</p>
+        <p className={styles.text}>Click below to see frequently asked questions by people experiencing eviction.</p>
+        <Button 
+          component={Link} 
+          href="/faq"
+          variant="contained"
+          sx={{ backgroundColor: '#ffc844', color: '#1d3747', borderRadius: '50px', padding: '12px 24px', fontWeight: 'bold', margin: '1rem' }}
+        >
           FAQs
         </Button>
 
-        <p className={styles.text}>Looking for resources?</p>
-        <Button color="primary" component={Link} href="/resource-finder">
+        <p className={styles.title}>Looking for resources?</p>
+        <p className={styles.text}>Click below to see our list of resources to help you avoid eviction.</p>
+        <Button 
+          component={Link} 
+          href="/resource-finder"
+          variant="contained"
+          sx={{ backgroundColor: '#ffc844', color: '#1d3747', borderRadius: '50px', padding: '12px 24px', fontWeight: 'bold', margin: '1rem' }}
+        >
           Resources
         </Button>
       </div>
