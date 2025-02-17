a712bf5900921bdaeddda04fe0e137ade39eed35
miacline <145244170+miacline@users.noreply.github.com>
2025-02-17 10:54:20 -0500

    Header design updates

--- eviction2app/src/app/components/Header.js
+++ eviction2app/src/app/components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Eviction Education</h1>
+      <h1 className={styles.title}>EvictionEducation</h1>
       <div className={styles.menuButton}>
         <button onClick={() => setMenuOpen(!menuOpen)}>
           {menuOpen ? <X size={24} /> : <Menu size={24} />}
@@ -23,6 +23,7 @@ export default function Header() {
         <Link href="/resource-finder" underline="hover" color=" #1d3747" className={styles.navLink}>Resources</Link>
         <Link href="/decision-finder" underline="hover" color=" #1d3747" className={styles.navLink}>Find A Solution</Link>
         <Link href="/faq" underline="hover" color=" #1d3747" className={styles.navLink}>FAQs</Link>
+        <Link href="/" underline="hover" color=" #1d3747" className={styles.navLink}>Translate</Link>
       </div>
       {menuOpen && (
         <motion.div 
@@ -33,6 +34,7 @@ export default function Header() {
           <Link href="/resource-finder" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Resources</Link>
           <Link href="/decision-finder" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Find A Solution</Link>
           <Link href="/faq" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>FAQs</Link>
+          <Link href="/" underline="hover" color="#1d3747" className={styles.mobileMenuLink}>Translate</Link>
         </motion.div>
       )}
     </header>
