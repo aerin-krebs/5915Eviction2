5ca41244696b9f4e8f2eb4673b22010b27a7f395
Natalie Chmura <ntchmura@gmail.com>
2025-03-25 10:49:43 -0400

    Adding: Spring endpoints
    Adds spring endpoints for everything but the faq and node resources

---
+++ evictionEducation/evictionEduspring/src/main/java/com/example/evictioneduspring/controllers/DecisionTreeController.java
@@ -0,0 +1,30 @@
+package com.example.evictioneduspring.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.evictioneduspring.entities.DecisionNode;
+import com.example.evictioneduspring.service.DecisionNodeService;
+import com.example.evictioneduspring.repositories.DecisionNodeRepository;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tree")
+public class DecisionTreeController {
+
+    @Autowired private DecisionNodeService decisionNodeService;
+
+    private final DecisionNodeRepository decisionNodeRepository;
+
+    public DecisionTreeController(DecisionNodeRepository decisionNodeRepository) {
+        this.decisionNodeRepository = decisionNodeRepository;
+    }
+
+    @GetMapping("/{id}")
+    public DecisionNode getDecisionTreeNodeById(@PathVariable Long id) {
+        return decisionNodeRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+}
---
+++ evictionEducation/evictionEduspring/src/main/java/com/example/evictioneduspring/controllers/QuestionAnswerController.java
@@ -0,0 +1,27 @@
+package com.example.evictioneduspring.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.evictioneduspring.entities.QuestionAnswer;
+import com.example.evictioneduspring.repositories.QuestionAnswerRepository;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionAnswerController {
+
+    private final QuestionAnswerRepository questionAnswerRepository;
+
+    public QuestionAnswerController(QuestionAnswerRepository questionAnswerRepository) {
+        this.questionAnswerRepository = questionAnswerRepository;
+    }
+
+    @GetMapping("/{id}") // DEBUG
+    public QuestionAnswer getQuestionAnswerById(@PathVariable Long id) {
+        return questionAnswerRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+}
