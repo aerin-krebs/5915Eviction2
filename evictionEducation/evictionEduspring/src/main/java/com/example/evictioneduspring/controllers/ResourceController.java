4a6e5ae72d96b82b276b42fa8ea5bc286f1bf1fb
Natalie Chmura <ntchmura@gmail.com>
2025-03-30 17:33:41 -0400

    Add: List fetch for FAQ resources

--- evictionEducation/evictionEduspring/src/main/java/com/example/evictioneduspring/controllers/ResourceController.java
+++ evictionEducation/evictionEduspring/src/main/java/com/example/evictioneduspring/controllers/ResourceController.java
@@ -4,9 +4,9 @@ import com.example.evictioneduspring.entities.Resource;
 import com.example.evictioneduspring.entities.FaqResource;
 import com.example.evictioneduspring.entities.NodeResource;
 import com.example.evictioneduspring.service.ResourceService;
+import com.example.evictioneduspring.service.FaqResourceService;
 import com.example.evictioneduspring.repositories.FaqResourceRepository;
 import com.example.evictioneduspring.repositories.NodeResourceRepository;
-
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class ResourceController {
 
     @Autowired private ResourceService resourceService;
+    @Autowired private FaqResourceService faqResourceService;
 
     private final NodeResourceRepository nodeResourceRepository;
     private final FaqResourceRepository faqResourceRepository;
@@ -40,5 +41,10 @@ public class ResourceController {
     @GetMapping("/faq/{faq_id}")
     public FaqResource getFaqResourceById(@PathVariable Long faq_id) {
         return faqResourceRepository.findById(faq_id).orElseThrow(RuntimeException::new);
+    }
+
+    @GetMapping("/faq-list")
+    public List<FaqResource> getFaqlResourceList() {
+        return faqResourceService.fetchFaqResourceList(); 
     }
 }
